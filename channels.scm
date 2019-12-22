81032dca2c4a211ef12cfa0f1bccbea24d160128
Oleg Pykhalov <go.wigust@gmail.com>
2019-12-22 12:15:29 +0300

    Add Jenkinsfile.
    * Jenkinsfile: New file.
    * channels.scm: New file.

---
+++ channels.scm
@@ -0,0 +1,11 @@
+;; Add my personal packages to those Guix provides.
+(cons* (channel
+        (name 'guix-wigust)
+        (url "https://cgit.duckdns.org/git/guix/guix-wigust"))
+       (channel
+        (name 'guix-wigust-services)
+        (url "https://cgit.duckdns.org/git/guix/guix-wigust-services"))
+       (channel
+        (name 'guix-tome4)
+        (url "https://cgit.duckdns.org/git/guix/guix-tome4"))
+       %default-channels)
