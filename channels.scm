0da837941ef47ae4907114ecf8fb79ddf0e7f59e
Oleg Pykhalov <go.wigust@gmail.com>
2020-01-04 16:44:58 +0300

    Jenkinsfile: Update.

--- channels.scm
+++ channels.scm
@@ -6,6 +6,9 @@
         (name 'guix-wigust-services)
         (url "https://cgit.duckdns.org/git/guix/guix-wigust-services"))
        (channel
+        (name 'guix-majordomo)
+        (url "https://cgit.duckdns.org/git/guix/guix-majordomo"))
+       (channel
         (name 'guix-tome4)
         (url "https://cgit.duckdns.org/git/guix/guix-tome4"))
        %default-channels)
